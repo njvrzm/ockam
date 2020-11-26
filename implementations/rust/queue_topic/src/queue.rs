5e5ff33982f505a22d5adbe789a1e1505c02d2da
Jared Stanbrough <jared@ockam.io>
2020-11-25 16:11:23 -0800

    Rewriting Topics to use Queues

--- implementations/rust/queue_topic/src/queue.rs
+++ implementations/rust/queue_topic/src/queue.rs
@@ -1,21 +1,20 @@
 use std::collections::HashMap;
-use std::cell::RefCell;
+use std::cell::{RefCell, RefMut};
 use std::rc::Rc;
-use std::borrow::{Borrow, BorrowMut};
 
 pub struct QueueMessage {
     pub body: Vec<u8>,
 }
 
 impl QueueMessage {
-    fn new(body: Vec<u8>) -> QueueMessage {
+    pub fn new(body: Vec<u8>) -> QueueMessage {
         QueueMessage {
             body
         }
     }
 }
 
-trait Queue {
+pub trait Queue {
     fn address(&self) -> String;
     fn enqueue(&mut self, message: QueueMessage);
     fn dequeue(&mut self) -> Option<QueueMessage>;
@@ -55,25 +54,34 @@ impl Queue for MemQueue {
     }
 }
 
-struct MemQueueWorker {
-   queue_map: HashMap<String, RefCell<Box<dyn Queue>>>
+pub struct MemQueueWorker {
+   queue_map: HashMap<String, Rc<RefCell<Box<dyn Queue>>>>
+}
+
+pub trait QueueWorker {
+    fn get_queue(&mut self, queue_address: &str) -> Option<Rc<RefCell<Box<dyn Queue>>>>;
+
+    fn remove_queue(&mut self, queue_name: &str);
 }
 
 impl MemQueueWorker {
-    fn new() -> MemQueueWorker {
+    pub fn new() -> MemQueueWorker {
         MemQueueWorker {
             queue_map: HashMap::new()
         }
     }
+}
 
-    fn get_queue(&mut self, queue_name: &str) -> Option<&RefCell<Box<dyn Queue>>> {
-        if self.queue_map.contains_key(queue_name) {
-            self.queue_map.get(queue_name)
+impl QueueWorker for MemQueueWorker {
+
+    fn get_queue(&mut self, queue_address: &str) -> Option<Rc<RefCell<Box<dyn Queue>>>> {
+        if self.queue_map.contains_key(queue_address) {
+            self.queue_map.get(queue_address).cloned()
         } else {
-            let name_string = queue_name.to_string();
-            let new_queue = RefCell::new(Box::new(MemQueue::new(name_string.clone())));
+            let name_string = queue_address.to_string();
+            let new_queue : Rc<RefCell<Box<dyn Queue>>> = Rc::new(RefCell::new(Box::new(MemQueue::new(name_string.clone()))));
             self.queue_map.insert(name_string.clone(), new_queue);
-            self.queue_map.get(queue_name)
+            self.queue_map.get(queue_address).cloned()
         }
     }
 
@@ -85,19 +93,20 @@ impl MemQueueWorker {
 #[test]
 fn queue_tdd() {
     let queue_worker = RefCell::new(MemQueueWorker::new());
-    let queue_name = "test";
+    let queue_address = "test";
     {
         let mut qw = queue_worker.borrow_mut();
-        let mut queue = qw.get_queue(queue_name).unwrap().borrow_mut();
+        let mut queue: Rc<RefCell<Box<dyn Queue>>> = qw.get_queue(queue_address).unwrap();
 
-        queue.enqueue(QueueMessage::new("A".as_bytes().to_vec()));
-        queue.enqueue(QueueMessage::new("B".as_bytes().to_vec()));
-        queue.enqueue(QueueMessage::new("C".as_bytes().to_vec()));
+        let mut rm = queue.borrow_mut();
+        rm.enqueue(QueueMessage::new("A".as_bytes().to_vec()));
+    //    queue.enqueue(QueueMessage::new("B".as_bytes().to_vec()));
+//        queue.enqueue(QueueMessage::new("C".as_bytes().to_vec()));
 
-        let out_message = queue.dequeue().unwrap();
-        println!("{:?}", out_message.body);
+      //  let out_message = queue.dequeue().unwrap();
+     //   println!("{:?}", out_message.body);
     }
 
-    let mut qw = queue_worker.borrow_mut();
-    qw.borrow_mut().remove_queue(queue_name);
+  /*  let mut qw = queue_worker.borrow_mut();
+    qw.borrow_mut().remove_queue(queue_address);*/
 }
--- implementations/rust/queue_topic/src/topic.rs
+++ implementations/rust/queue_topic/src/topic.rs
@@ -1,161 +1,119 @@
-use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
-
-#[derive(Copy, Clone)]
-pub struct TopicMessage<'a> {
-    pub body: &'a [u8],
-}
+use crate::queue::*;
+use std::cell::RefCell;
 
 pub trait Topic {
-    fn name(&self) -> String;
+    fn topic_address(&self) -> String;
 }
 
 pub struct MemTopic {
-    pub topic_name: String,
+    pub topic_address: String,
 }
 
 impl MemTopic {
     pub fn new(topic_name: String) -> TopicHandle {
-        Rc::new(Box::new(MemTopic { topic_name }))
+        Rc::new(Box::new(MemTopic { topic_address: topic_name }))
     }
 }
 
 impl Topic for MemTopic {
-    fn name(&self) -> String {
-        self.topic_name.clone()
+    fn topic_address(&self) -> String {
+        self.topic_address.clone()
     }
 }
 
-pub type MessageHandler = Box<dyn Fn(&TopicMessage) -> ()>;
 pub type TopicHandle = Rc<Box<dyn Topic>>;
+pub type QueueHandle = Rc<Box<dyn Queue>>;
 
 pub struct TopicSubscription {
     pub topic: TopicHandle,
-    pub id: usize,
+    pub queue: QueueHandle,
+    pub subscriber_address: String,
 }
 
 pub trait Subscription {
     fn topic(&self) -> TopicHandle;
 
-    fn id(&self) -> usize;
+    fn queue(&self) -> QueueHandle;
+
+    fn subscriber_address(&self) -> String;
 }
 
-pub(crate) type SubscriptionHandle = Rc<RefCell<dyn Subscription>>;
-
 impl Subscription for TopicSubscription {
-    fn topic(&self) -> Rc<Box<dyn Topic>> {
+    fn topic(&self) -> TopicHandle {
         self.topic.clone()
     }
 
-    fn id(&self) -> usize {
-        self.id
+    fn queue(&self) -> QueueHandle {
+        self.queue.clone()
     }
-}
 
-pub trait TopicCreator {
-    fn create_topic(&mut self, topic_name: String);
-
-    fn get_topic(&self, topic_name: String) -> Option<&TopicHandle>;
+    fn subscriber_address(&self) -> String {
+        self.subscriber_address.clone()
+    }
 }
 
 pub trait TopicWorker {
-    fn publish(&mut self, topic_name: String, message: TopicMessage);
+    fn publish(&mut self, topic: &str, message: QueueMessage);
 
-    fn subscribe(&mut self, name: String) -> Option<SubscriptionHandle>;
+    fn subscribe(&mut self, topic: &str) -> Option<String>;
 
-    fn poll(&mut self, handle: SubscriptionHandle, message_handler: &dyn Fn(TopicMessage) -> ());
+    fn poll(&mut self, subscriber: &str, message_handler: &dyn Fn(QueueMessage));
 
-    fn unsubscribe(&mut self, handle: SubscriptionHandle);
+    fn unsubscribe(&mut self, subscriber: &str);
 }
 
-struct LocalTopicManager {
-    topics: HashMap<String, TopicHandle>,
-    subscriptions: Vec<SubscriptionHandle>,
+struct MemTopicWorker {
+    queue_worker: RefCell<Box<dyn QueueWorker>>,
+    subscriptions: HashMap<String, Rc<RefCell<Box<dyn Queue>>>>,
     subscription_id_counter: usize,
 }
 
-impl LocalTopicManager {
-    fn new() -> LocalTopicManager {
-        LocalTopicManager {
-            topics: HashMap::new(),
-            subscriptions: Vec::new(),
+impl MemTopicWorker {
+    fn new(queue_worker: RefCell<Box<dyn QueueWorker>>) -> MemTopicWorker {
+        MemTopicWorker {
+            subscriptions: HashMap::new(),
             subscription_id_counter: 0,
+            queue_worker
         }
     }
 }
 
-impl TopicCreator for LocalTopicManager {
-    fn create_topic(&mut self, topic_name: String) {
-        let topic: Rc<Box<dyn Topic>> = Rc::new(Box::new(MemTopic {
-            topic_name: topic_name.clone(),
-        }));
-        self.topics.insert(topic_name.clone(), topic);
-    }
-
-    fn get_topic(&self, topic_name: String) -> Option<&TopicHandle> {
-        self.topics.get(topic_name.as_str())
-    }
-}
-
-impl TopicWorker for LocalTopicManager {
-    fn publish(&mut self, topic_name: String, message: TopicMessage) {
-        for subscription in &self.subscriptions {
-            if (*subscription.borrow())
-                .topic()
-                .name()
-                .eq(topic_name.clone().as_str())
-            {
-                // TODO put on internal Queue
-            }
-        }
-    }
-
-    fn subscribe(&mut self, topic_name: String) -> Option<SubscriptionHandle> {
-        let maybe_topic = self.get_topic(topic_name);
-
-        match maybe_topic {
-            Some(topic) => {
-                let subscription: SubscriptionHandle = Rc::new(RefCell::new(TopicSubscription {
-                    topic: (*topic).clone(),
-                    id: self.subscription_id_counter,
-                }));
-                self.subscriptions.push(subscription.clone());
-                self.subscription_id_counter += 1;
-                Some(subscription.clone())
-            }
-            None => None,
-        }
-    }
-
-    fn poll(&mut self, handle: SubscriptionHandle, func: &dyn Fn(TopicMessage) -> ()) {
+impl TopicWorker for MemTopicWorker {
+    fn publish(&mut self, topic: &str, message: QueueMessage) {
         unimplemented!()
     }
 
-    fn unsubscribe(&mut self, handle: SubscriptionHandle) {
-        self.subscriptions
-            .retain(|s| s.borrow().id() != handle.borrow().id())
+    fn subscribe(&mut self, topic_address: &str) -> Option<String> {
+        let subscriber_address = format!("{}_{}", self.subscription_id_counter, topic_address);
+        println!("{}", subscriber_address);
+
+        match self.queue_worker.borrow_mut().get_queue(subscriber_address.as_str()) {
+            Some(queue) => {
+                self.subscriptions.insert(subscriber_address.clone(), queue);
+                self.subscription_id_counter += 1;
+                Some(subscriber_address.clone())
+            },
+            None => None
+        }
+    }
+
+    fn poll(&mut self, subscriber: &str, message_handler: &dyn Fn(QueueMessage)) {
+        unimplemented!()
+    }
+
+    fn unsubscribe(&mut self, subscriber: &str) {
+        unimplemented!()
     }
 }
 
 #[test]
 fn topic_tdd() {
-    let mut topic_manager = LocalTopicManager::new();
+    let queue_worker : RefCell<Box<dyn QueueWorker>> = RefCell::new(Box::new(MemQueueWorker::new()));
+    let topic_worker = RefCell::new(MemTopicWorker::new(queue_worker));
 
-    topic_manager.create_topic("topic1".to_string());
-    topic_manager.create_topic("topic2".to_string());
 
-    let subscription1 = topic_manager.subscribe("topic1".to_string()).unwrap();
-
-    let subscription2 = topic_manager.subscribe("topic2".to_string()).unwrap();
-
-    let body = [1, 2, 3];
-
-    let message = TopicMessage { body: &(body) };
-
-    topic_manager.publish("topic1".to_string(), message);
-    topic_manager.publish("topic2".to_string(), message);
-
-    topic_manager.unsubscribe(subscription1);
-    topic_manager.unsubscribe(subscription2);
+    let sub = topic_worker.borrow_mut().subscribe("test").unwrap();
+    println!("{}", sub)
 }
